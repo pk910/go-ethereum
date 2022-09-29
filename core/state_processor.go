4f86e3b26820b22f003758756ac2c1f11055c854
pk910 <philipp@pk910.de>
2022-09-29 22:53:29 +0200

    fix

--- core/state_processor.go
+++ core/state_processor.go
@@ -95,7 +95,7 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 	// Finalize the block, applying any consensus engine specific extras (e.g. block rewards)
 	p.engine.Finalize(p.bc, header, statedb, block.Transactions(), block.Uncles())
 
-	pklogger.PKLoggerSend(fmt.Sprintf("block end: number=%d\n", blockHash, blockNumber));
+	pklogger.PKLoggerSend(fmt.Sprintf("block end: number=%d\n", blockNumber));
 
 	return receipts, allLogs, *usedGas, nil
 }
